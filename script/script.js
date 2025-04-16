ae827bcb8b6162ee1bfc4705a9061353e94057d2
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-04-16 16:08:53 +0300

    HW-06-02

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const openBtn = document.querySelector('.menu-open-btn');  
+const closeBtn = document.querySelector('.button-mob-menu');  
+const mobileMenu = document.querySelector('.mob-menu');  
+
+openBtn.addEventListener('click', () => {  
+  mobileMenu.classList.add('active');  
+});  
+
+closeBtn.addEventListener('click', () => {  
+  mobileMenu.classList.remove('active');  
+});  
